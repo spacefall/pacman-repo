c27fc68bc8bfb6cdb2dc209b0c9b1c3e890a67d2
spacefall <lucarichu@outlook.it>
2024-01-07 21:47:18 +0100

    repacks: switched to extra/electron instead of building electron-bin

--- vencord-desktop-git-patch/vesktop.sh
+++ vencord-desktop-git-patch/vesktop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec electron-bin /usr/lib/vencord/app.asar \
+exec electron /usr/lib/vencord/app.asar \
 			 --ozone-platform-hint=auto \
 			 --enable-features=WaylandWindowDecorations \
 			 "${flags[@]}" "$@"
